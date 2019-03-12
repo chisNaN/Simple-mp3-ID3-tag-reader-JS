724c98d5e47168cc00243a50adaeff45132921d0
FoxCode <m1sterfinch@yandex.ru>
2019-03-12 12:04:52 +0300

    add more genries

--- app.js
+++ app.js
@@ -17,7 +17,16 @@ var ID3Reader = function(file){
         "Avantgarde","Gothic Rock","Progressive Rock","Psychedelic Rock","Symphonic Rock","Slow Rock","Big Band","Chorus","Easy Listening",
         "Acoustic","Humour","Speech","Chanson","Opera","Chamber Music","Sonata","Symphony","Booty Bass","Primus","Porn Groove","Satire",
         "Slow Jam","Club","Tango","Samba","Folklore","Ballad","Power Ballad","Rhythmic Soul","Freestyle","Duet","Punk Rock","Drum Solo",
-        "A capella","Euro-House","Dance Hall"];
+        "A capella","Euro-House","Dance Hall","Goa","Drum & Bass","Club-House","Hardcore Techno","Terror","Indie","BritPop","Negerpunk",
+        "Polsk Punk","Beat","Christian Gangsta Rap","Heavy Metal","Black Metal","Crossover",
+        "Contemporary Christian","Christian rock","Merengue","Salsa","Thrash Metal","Anime","Jpop",
+        "Synthpop","Abstract","Art Rock","Baroque","Bhangra","Big beat","Breakbeat","Chillout",
+        "Downtempo","Dub","EBM","Eclectic","Electro","Electroclash","Emo","Experimental","Garage",
+        "Global","IDM","Illbient","Industro-Goth","Jam Band","Krautrock","Leftfield","Lounge",
+        "Math Rock","New Romantic","Nu-Breakz","Post-Punk","Post-Rock","Psytrance","Shoegaze",
+        "Space Rock","Trop Rock","World Music","Neoclassical","Audiobook","Audio theatre",
+        "Neue Deutsche Welle","Podcast","Indie-Rock","G-Funk","Dubstep","Garage Rock","Psybient"
+    ];
 }
 // Чтение файла побитово
 ID3Reader.prototype.read = function(encodeType,callback){
@@ -75,13 +84,11 @@ ID3Reader.prototype.STRING_DECODE = function(encode, data){
     return decoder.decode(bytes).trim();
 }
 ID3Reader.prototype.GET_GENRE = function(number){
-    
-    return this.GENRES[number];
+    return (this.GENRES[number]) ? this.GENRES[number] : 'no genre' ;
 }
 
 
-button.onclick = function(){
-        
+button.onclick = function(){  
     let file = new ID3Reader(fileElm.files[0]);
     file.read('cp1251',function(data){
         console.log(data)
